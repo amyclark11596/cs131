ddf4ca5fdbb0085dbe8b84e233e780d3247d8ad0
amyclark11596 <amyclark11596@gmail.com>
2018-10-05 11:23:22 -0400

    part 1 complete

--- PA1_2 starter kit/src/cs131/pa1/filter/concurrent/ConcurrentREPL.java
+++ PA1_2 starter kit/src/cs131/pa1/filter/concurrent/ConcurrentREPL.java
@@ -18,9 +18,12 @@ public class ConcurrentREPL {
 			command = s.nextLine();
 			if(command.equals("exit")) {
 				break;
+			} else if(command.endsWith("&")){
+				//put checks for background r
 			} else if(!command.trim().equals("")) {
 				//building the filters list from the command
-				ConcurrentFilter filterlist = ConcurrentCommandBuilder.createFiltersFromCommand(command);
+				ConcurrentFilter filterlist;
+				filterlist = ConcurrentCommandBuilder.createFiltersFromCommand(command);
 				while(filterlist != null) {
 					filterlist.process();
 					filterlist = (ConcurrentFilter) filterlist.getNext();
@@ -29,6 +32,18 @@ public class ConcurrentREPL {
 		}
 		s.close();
 		System.out.print(Message.GOODBYE);
+	}
+	
+	//iterates through the list and checks each process for completion. Returns false if one is not finished
+	public static boolean isDone(ConcurrentFilter filterlist){
+		while (filterlist.isDone()){
+			if(filterlist.getNext()!=null){
+				filterlist = (ConcurrentFilter) filterlist.getNext();
+			}
+			return true;
+		}
+			
+		return false;
 	}
 
 }
